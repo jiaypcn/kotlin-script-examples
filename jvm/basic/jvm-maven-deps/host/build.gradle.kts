1031a5e241876a06e1a2aa3bfee47c3b366ef1c5
Terry <jiaypcn@qq.com>
2025-07-20 19:02:43 +0800

    look print

--- jvm/basic/jvm-maven-deps/host/build.gradle.kts
+++ jvm/basic/jvm-maven-deps/host/build.gradle.kts
@@ -1,6 +1,6 @@
-
 plugins {
     kotlin("jvm")
+    application
 }
 
 val kotlinVersion: String by rootProject.extra
@@ -12,5 +12,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-scripting-jvm-host:$kotlinVersion")
     testImplementation("junit:junit:4.13.1")
     testRuntimeOnly("org.slf4j:slf4j-nop:1.7.28")
+}
+
+application {
+    mainClass.set("org.jetbrains.kotlin.script.examples.jvm.resolve.maven.host.HostKt")
 }
 
